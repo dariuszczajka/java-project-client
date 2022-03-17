537174a02862ec4dcc7e9891566ac7f64e795158
darek <dczajka@tuta.io>
2022-03-17 22:01:00 +0100

    table, axios and 3 endpoint support

--- src/components/ColumnForm.jsx
+++ src/components/ColumnForm.jsx
@@ -1,21 +1,78 @@
 import Form from "react-bootstrap/Form";
-import {Button} from "react-bootstrap";
+import {Button, Table} from "react-bootstrap";
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
 
 const ColumnForm = () => {
+
+    const [table, setTable] = useState(null);
+
+    useEffect(() => {
+        axios.get('https://java-project-server.herokuapp.com/api/rows/repeating')
+            .then(res => {
+                setTable(res.data);
+            })
+    },[]);
+
+    function handleChange(e){
+        console.log(e.target.value)
+        if(e.target.value === 'distinct'){
+            axios.get('https://java-project-server.herokuapp.com/api/rows/distinct')
+                .then(res => {
+                    setTable(res.data);
+                })
+        }
+        if(e.target.value === 'repetitive'){
+            axios.get('https://java-project-server.herokuapp.com/api/rows/repeating')
+                .then(res => {
+                    setTable(res.data);
+                })
+        } else
+        if(e.target.value === 'all'){
+            axios.get('https://java-project-server.herokuapp.com/api/rows/')
+                .then(res => {
+                    setTable(res.data);
+                })
+        }
+    }
+
     return(
-        <Form>
-            <Form.Group className="mb-3">
-                <Form.Label htmlFor="select">Select the column</Form.Label>
-                <Form.Select id="select">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </Form.Select>
-            </Form.Group>
-            <Button type="submit">Submit</Button>
-        </Form>
+        <div className={'root'}>
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Label htmlFor="select">Select the column</Form.Label>
+                    <Form.Select id="select" onChange={handleChange}>
+                        <option value={"all"}>All</option>
+                        <option value={"distinct"}>Distinct</option>
+                        <option value={"repetitive"}>Repetitive</option>
+                    </Form.Select>
+                </Form.Group>
+            </Form>
+
+            <Table striped bordered hover size="sm" className={'table'}>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Kolumna1</th>
+                    <th>Kolumna2</th>
+                    <th>Kolumna3</th>
+                    <th>Kolumna4</th>
+                </tr>
+                </thead>
+                <tbody>
+                {table !== null && table.map(row => { return(
+                    <tr>
+                        <td>{row.id}</td>
+                        <td>{row.kolumna1}</td>
+                        <td>{row.kolumna2}</td>
+                        <td>{row.kolumna3}</td>
+                        <td>{row.kolumna4}</td>
+                    </tr>
+                )
+                })}
+                </tbody>
+            </Table>
+        </div>
     )
 }
 export default ColumnForm
